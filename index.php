c1ff5748a9519544427a1b1d9acb7c36313140e3
Miguel Peixe <miguel@cardume.art.br>
2014-08-08 19:58:55 -0300

    remove header and footer, put it all on index

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
-<?php get_header(); ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo('charset'); ?>" />
+<title><?php
+	global $page, $paged;
 
-<div ui-view></div>
+	wp_title( '|', true, 'right' );
 
-<?php get_footer(); ?>
+	bloginfo( 'name' );
+
+	$site_description = get_bloginfo('description', 'display');
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		echo " | $site_description";
+
+	if ( $paged >= 2 || $page >= 2 )
+		echo ' | ' . __('Page', 'bikeit') . max($paged, $page);
+
+	?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>" />
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico" type="image/x-icon" />
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(get_bloginfo('language')); ?>>
+
+	<div ui-view></div>
+
+	<?php wp_footer(); ?>
+
+</body>
+</html>
