1fdf45dbd9695b168fd9aa4fa5273be5cdb7f40e
Miguel Peixe <miguel@cardume.art.br>
2016-06-14 11:03:08 -0300

    update ga

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 window._gaq = _gaq || [];
-_gaq.push(['_setAccount', "UA-79265810-1"]);
-_gaq.push(['_setDomainName', 'bikeit.com.br']);
+window._gaq.push(['_setAccount', "UA-79265810-1"]);
+window._gaq.push(['_setDomainName', 'bikeit.com.br']);
 (function() {
 	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
